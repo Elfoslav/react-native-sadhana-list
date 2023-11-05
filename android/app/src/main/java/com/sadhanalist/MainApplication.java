acae2493c070dfb26795cc0a6e27608ccfd6e2d2
Tomas <tom.hromnik@gmail.com>
2023-11-05 08:19:51 +0100

    Fix codepush

--- android/app/src/main/java/com/sadhanalist/MainApplication.java
+++ android/app/src/main/java/com/sadhanalist/MainApplication.java
@@ -1,6 +1,7 @@
 package com.sadhanalist;
 
 import android.app.Application;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
