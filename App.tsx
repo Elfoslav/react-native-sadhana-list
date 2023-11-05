a3b6c73390db76d9970f6d53c4ebb71179ac385e
Tomas <tom.hromnik@gmail.com>
2023-11-05 07:39:55 +0100

    Add appcenter & codepush

--- App.tsx
+++ App.tsx
@@ -6,10 +6,15 @@
  */
 
 import React from 'react';
+import codePush from 'react-native-code-push';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeView from './src/views/HomeView';
 import SadhanaListView from './src/views/SadhanaListView';
+
+// If you want your app to discover updates more quickly, you can also choose to sync up with the CodePush server
+// every time the app resumes from the background.
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 
 function App(): JSX.Element {
   const Stack = createNativeStackNavigator();
@@ -24,4 +29,4 @@ function App(): JSX.Element {
   );
 }
 
-export default App;
+export default codePush(codePushOptions)(App);
